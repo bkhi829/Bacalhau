b9f9324dbc30e9099449a0f6b13cee7f60e3250a
Brian <briankhi829@gmail.com>
2021-05-06 16:47:24 -0700

    Changed to correct API

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-export const fetchAllMembers = () => axios.get(process.env.REACT_APP_API_URL+"/bacalhauAPI/members");
+export const fetchAllMembers = () => axios.get(process.env.REACT_APP_API_URL+"/members");
 
-export const fetchVideoTypes = () => axios.get(process.env.REACT_APP_API_URL+"/bacalhauAPI/videos");
+export const fetchVideoTypes = () => axios.get(process.env.REACT_APP_API_URL+"/videos");
 
 export const fetchVideos = (type,nextPageToken) => axios.get(`https://youtube.googleapis.com/youtube/v3/playlistItems?part=snippet&maxResults=20&playlistId=${type}${nextPageToken!==null?'&pageToken='+nextPageToken:""}&key=${process.env.REACT_APP_YOUTUBE_API_KEY}`);
